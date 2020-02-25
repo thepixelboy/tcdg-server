b512b2e0a80bccbaec38c2d6fdbf85e193ef1cfe
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-02-25 15:44:00 +0100

    using an Express Router and parsing form bodies

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-    <div>
-      <h1>Hi there!</h1>
-    </div>
-  `);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
