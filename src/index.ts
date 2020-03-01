11e36980570fb16b107e16ff24f2916dc5e1a334
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-03-01 12:10:30 +0100

    Defining a RouteBinder

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express, { Request, Response } from 'express';
-import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+
+import { router } from './routes/loginRoutes';
+import { AppRouter } from './AppRouter';
+import './controllers/LoginController';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['adsdada'] }));
 app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
