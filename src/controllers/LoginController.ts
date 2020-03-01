9f3c295ad3eec632687e6c96ea8b07645944e6d5
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-03-01 11:28:59 +0100

    Implementing Get and Controller decorators

---
+++ src/controllers/LoginController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { get } from './decorators/routes';
+
+@controller('/')
+class LoginController {
+  @get('/login')
+  getLogin(req: Request, res: Response): void {
+    if (req.session && req.session.loggedIn) {
+      res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+    } else {
+      res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login">Login</a>
+      </div>
+    `);
+    }
+  }
+}
