159e6a9a0f9a4b03e3cb6fed645284a498ed35cf
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-03-02 17:12:30 +0100

    Fixing routes

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,14 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 
-import { router } from './routes/loginRoutes';
 import { AppRouter } from './AppRouter';
 import './controllers/LoginController';
+import './controllers/RootController';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['adsdada'] }));
-app.use(router);
 app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
